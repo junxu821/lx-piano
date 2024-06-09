4b3781cf3fb810ce876dfe04ae50251769f4eb69
junxu821 <xu.jun.work@qq.com>
2024-06-09 23:57:39 +0800

    update: 更换部署公共路径

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,7 +1,7 @@
 import { Component } from "vue";
 export default [{
   
-    path: "/lx-piano",
+    path: "/",
     name: "index",
     component: (): Component => import("@/views/PianoPc/index.vue"),
     mata: {
