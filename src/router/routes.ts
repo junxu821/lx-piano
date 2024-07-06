b73d0936e3bc19f8864478b8750729491c4b3ce2
junxu821 <xu.jun.work@qq.com>
2024-07-06 23:52:51 +0800

    update: 更改路由路径

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,7 +1,7 @@
 import { Component } from "vue";
 export default [{
   
-    path: "/",
+    path: "/xu-piano",
     name: "index",
     component: (): Component => import("@/views/PianoPc/index.vue"),
     mata: {
