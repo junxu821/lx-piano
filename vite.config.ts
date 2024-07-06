bf59442048076c8c8eca747eedad717c34f26919
junxu821 <xu.jun.work@qq.com>
2024-07-06 23:22:18 +0800

    update: 适配https

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // base:'/lx-piano/',
+  base:'/xu-piano/',
   resolve: {
     // 配置路径别名
     alias: {
